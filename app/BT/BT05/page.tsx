72400c319e01687b0de64e49dceeb551773af440
xuannhat <xuannhatvn211@gmail.com>
2024-08-20 07:47:55 -0700

    BT

---
+++ app/BT/BT05/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Button from "@/app/components/Button";
+export default function page() {
+  return (
+    <div style={{ display: "flex" }}>
+      <h4> Bài 5: </h4> <Button></Button>
+    </div>
+  );
+}
